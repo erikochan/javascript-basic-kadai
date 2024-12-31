8c5d318cdc8e669e7ca00b7904f5fa5fbf65c8c5
Eriko Tsuyuki <eritsu131821@gmail.com>
2024-12-31 04:18:37 -0800

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+const today = new Date();
+
+//年、月、日を取得
+const year = today.getFullYear();
+const  month = today.getMonth() +1;
+const day = today.getDate();
+console.log(year + '年' + month + '月' + day + '日');
